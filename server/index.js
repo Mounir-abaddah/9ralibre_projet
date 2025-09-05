5871049b8cee5ed3374091bba58ff296b06ac915
Mounir abaddah <mounirabaddah6@gmail.com>
2025-09-05 18:39:52 +0100

    :sparkles: feat: ajout du routeur d'authentification et correction de la configuration de la base de données

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const app = express();
 require('dotenv').config();
 
 app.use(cors());
+app.use(express.json())
 
-require('./config/dbConfig')
+require('./config/dbConfig');
+
+const authRouter = require('./routes/AuthRoute');
+app.use('/auth',authRouter)
 
 const port = process.env.PORT;
 app.listen(port,()=>{
