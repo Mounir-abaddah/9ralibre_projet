f4f72a9709aef25bbffd0e16bcc339f2643728f7
Mounir abaddah <mounirabaddah6@gmail.com>
2025-09-12 20:03:16 +0100

    :sparkles: feat: intégration de registerShema (Zod) dans la route /register et amélioration des messages d'erreu

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -2,26 +2,34 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel');
 const bcrypt = require('bcryptjs');
-
+const registerShema = require('../validations/authValidation');
 
 router.post('/register',async(req,res)=>{
     try{
-        const emailExists = await User.findOne({email:req.body.email})
+        const registerValidation = registerShema.registerShema.parse(req.body)
+        const emailExists = await User.findOne({email:registerValidation.email})
         if(emailExists){
-            return res.status(400).send({message:'Email exists deja',success:false})
+            return res.status(400).send({message:'Impossible de créer un compte avec ces informations',success:false})
         }
-        const hashedPassword = await bcrypt.hash(req.body.password,10);
+        const hashedPassword = await bcrypt.hash(registerValidation.password,10);
         const newUser = new User({
-            nom : req.body.nom,
-            prenom : req.body.prenom,
-            type : req.body.type,
-            email : req.body.email,
+            nom : registerValidation.nom,
+            prenom : registerValidation.prenom,
+            type : registerValidation.type,
+            email : registerValidation.email,
             password : hashedPassword,
         })
         await newUser.save();
-        return res.status(200).send({ message: "Votre compte a ete bien creer", success: true });
+        return res.status(200).send({ message: "Votre compte a été créé avec succès !", success: true });
     }catch(err){
-        res.status(400).send({message:'Une erreure est survenue',success:false})
+        if(err.name === "ZodError"){
+            return res.status(400).send({
+                message:"Validation échouée",
+                success:false,
+                error:err.issues.map(e => e.message)
+            })
+        }
+        res.status(500).send({message:'Une erreure est survenue',success:false})
     }
 })
 
