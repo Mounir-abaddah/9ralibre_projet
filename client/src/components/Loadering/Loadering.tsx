37a012579288c2a8f09252018e691104cc0191db
Mounir abaddah <mounirabaddah6@gmail.com>
2025-09-13 17:25:52 +0100

    :sparkles: feat: ajoutation  le composant Loading pour l'affichage en attente

---
+++ client/src/components/Loadering/Loadering.tsx
@@ -0,0 +1,11 @@
+import { Loader } from 'lucide-react';
+
+const Loadering = () => {
+  return (
+    <div className='animate-spin'>
+         <Loader />
+    </div>
+  )
+}
+
+export default Loadering
