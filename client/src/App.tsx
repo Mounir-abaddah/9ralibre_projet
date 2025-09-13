dde8c4262e10b071125ecacbf98a24c63fe0ef78
Mounir abaddah <mounirabaddah6@gmail.com>
2025-09-13 17:31:47 +0100

    :sparkles: feat: ajoutation du Routes du page du Home et Inscription et aussi un Toaster pour afficher des notifications temporaires à l’utilisateu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Inscription from './pages/Inscription/Inscription';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Toaster position='top-right' reverseOrder={false} />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/inscription' element={<Inscription />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+    
   )
 }
 
