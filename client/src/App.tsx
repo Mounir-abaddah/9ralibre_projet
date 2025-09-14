92d64ad2007da9b8c566b0e8426f740db991bd95
Mounir abaddah <mounirabaddah6@gmail.com>
2025-09-14 15:31:50 +0100

    :sparkles: feat: ajoutation du Route de connexion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Inscription from './pages/Inscription/Inscription';
 import { Toaster } from 'react-hot-toast';
+import Connexion from './pages/Connexion/Connexion';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/inscription' element={<Inscription />} />
+        <Route path='/connexion' element={<Connexion />} />
       </Routes>
     </BrowserRouter>
     </div>
