c3358cb5854dad5e66d9990d0ebdb1b8939093b2
Mounir abaddah <mounirabaddah6@gmail.com>
2025-09-14 13:46:40 +0100

    :sparkles: feat: ajout de la route connexion et correction de la validation register

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/UserModel');
 const bcrypt = require('bcryptjs');
-const registerShema = require('../validations/authValidation');
+const {registerShema , loginSchema} = require('../validations/authValidation');
+const jwt = require('jsonwebtoken');
+
 
 router.post('/register',async(req,res)=>{
     try{
-        const registerValidation = registerShema.registerShema.parse(req.body)
+        const registerValidation = registerShema.parse(req.body)
         const emailExists = await User.findOne({email:registerValidation.email})
         if(emailExists){
             return res.status(400).send({message:'Impossible de créer un compte avec ces informations',success:false})
@@ -22,6 +24,40 @@ router.post('/register',async(req,res)=>{
         await newUser.save();
         return res.status(200).send({ message: "Votre compte a été créé avec succès !", success: true });
     }catch(err){
+        if(err.name === "ZodError"){
+            return res.status(400).send({
+                message:"Validation échouée",
+                success:false,
+                error:err.issues.map(e => e.message)
+            })
+        }
+        res.status(500).send({message:'Une erreure est survenue',success:false})
+    }
+})
+
+
+router.post('/connexion',async(req,res)=>{
+    try{
+        const {email,password} = loginSchema.parse(req.body);
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).send({message:"Authfailed",success:false})
+        }
+        const passwordMatch = await bcrypt.compare(password,user.password)
+        if(!passwordMatch){
+            return res.status(400).send({message:"Mot de passe incorecte",success:false})
+        }
+        const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{
+            expiresIn : "1d"
+        });
+        res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 24 * 60 * 60 * 1000,
+        });
+        return res.status(200).send({message: "Connexion réussie",success: true,});
+  }catch(err){
         if(err.name === "ZodError"){
             return res.status(400).send({
                 message:"Validation échouée",
