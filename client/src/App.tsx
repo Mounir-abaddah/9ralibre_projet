19bdfd4fea426619a4427b3eb9f675ae3428b55c
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-09-19 23:28:21 +0100

    :sparkles: feat: ajoutation les route de oublier le mot de passe et modification du mot de passe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import Home from './pages/Home/Home';
 import Inscription from './pages/Inscription/Inscription';
 import { Toaster } from 'react-hot-toast';
 import Connexion from './pages/Connexion/Connexion';
+import OublierMotdepasse from './pages/OublierMotdepasse/OublierMotdepasse';
+import ModificationMotdepasse from './pages/ModificationMotdepasse/ModificationMotdepasse';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/inscription' element={<Inscription />} />
         <Route path='/connexion' element={<Connexion />} />
+        <Route path='/password/reset' element={<OublierMotdepasse />} />
+        <Route path='/password/reset/:token' element={<ModificationMotdepasse />} />
       </Routes>
     </BrowserRouter>
     </div>
