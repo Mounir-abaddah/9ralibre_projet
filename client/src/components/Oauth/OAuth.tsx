7fd97467d28d9c66e9f104b4114eae6c18889b02
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-09-21 20:32:07 +0100

    :sparkles: feat: ajout de la redirection vers l’URL OAuth (Google) dans le composant OAuth

--- client/src/components/Oauth/OAuth.tsx
+++ client/src/components/Oauth/OAuth.tsx
@@ -5,10 +5,14 @@ type OauthType = {
   text_2: string;
 };
 const OAuth = ({ text_1, text_2 }: OauthType) => {
+  const handleClick = ()=>{
+    const apiUrl = import.meta.env.VITE_OAUTH_URL;
+    window.location.href = apiUrl
+  }
   return (
     <div className="w-full">
       <div className="flex w-full justify-around lg:flex-row md:flex-row flex-col-reverse gap-3 ">
-        <div className="flex cursor-pointer w-full items-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:bg-slate-50 hover:inset-shadow-md">
+        <div onClick={handleClick} className="flex cursor-pointer w-full items-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:bg-slate-50 hover:inset-shadow-md">
           <Google size={19}/>
           <hr className="h-6 rounded-md border-1" />
           <p>{text_1}</p>
