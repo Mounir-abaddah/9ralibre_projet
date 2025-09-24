b5615f4a908f2e256577cebe745c0ac9f02c57cb
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-09-24 20:43:29 +0100

    :sparkles: feat: ajout du middleware de vérification du token JWT

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req,res,next)=>{
+    const authHeaders = req.headers["authorization"];
+    const token = authHeaders && authHeaders.split(" ")[1];
+    if (!token) {
+        return res.status(401).send({message: "⛔ Accès refusé : Token manquant",success: false});
+    }
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded;
+        next();
+    }catch(err){
+        return res.status(403).send({message: "❌ Token invalide ou expiré",success: false,error: err.message});
+    }
+}
+
+module.exports = authMiddleware
