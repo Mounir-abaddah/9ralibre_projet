73b73e03f916b2110f70c67e253c9812bf48b0f8
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-09-25 23:01:57 +0100

    :sparkles: feat: ajout du champ boolean AccountVerified dans le modèle User pour suivre la validation des comptes

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -21,6 +21,10 @@ const UserSchema = new mongoose.Schema({
         type:String,
         default : ""
     },
+    accountVerified:{
+        type:Boolean,
+        default:false
+    }
 },{
     timestamps:true
 })
