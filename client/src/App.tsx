f18b196558819228fca19b757d3e8d6af8e2c268
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-09-26 16:42:02 +0100

    :sparkles: feat: ajout de la page de vérification d'email pour activer le compte utilisateur

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 import Connexion from './pages/Connexion/Connexion';
 import OublierMotdepasse from './pages/OublierMotdepasse/OublierMotdepasse';
 import ModificationMotdepasse from './pages/ModificationMotdepasse/ModificationMotdepasse';
+import VerificationEmail from './pages/VerficationEmail/VerificationEmail';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/connexion' element={<Connexion />} />
         <Route path='/password/reset' element={<OublierMotdepasse />} />
         <Route path='/password/reset/:token' element={<ModificationMotdepasse />} />
+        <Route path='/inscription/confirm-email/:token' element={<VerificationEmail />} />
       </Routes>
     </BrowserRouter>
     </div>
