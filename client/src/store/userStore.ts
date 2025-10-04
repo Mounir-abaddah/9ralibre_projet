23d9fe17dceb4ecf87dfe5107f3569ef2c456a05
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-04 19:35:20 +0100

    :bug: fix: corriger le typage de l'état user dans le store Zustand (data en objet unique au lieu d'un tableau)

--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { create } from 'zustand';
 
 interface typedata{
-    id: number,
+    id: string,
     nom: string,
     prenom: string,
     email: string,
@@ -11,21 +11,28 @@ interface typedata{
 }
 
 interface typeAllData{
-    data:typedata[] | null
+    data:typedata
     loading:boolean
     error:string | null
     fetchData:()=>Promise<void>
 }
 
 export const useProtectedRoutes = create<typeAllData>()((set)=>({
-    data:null,
+    data:{
+        id:"",
+        nom:"",
+        prenom:"",
+        email:"",
+        role:"",
+        accountVerified:true
+    },
     loading:true,
     error:null,
     fetchData:async()=>{
         try{
             const apiUrl = import.meta.env.VITE_API_URL;
             const response = await axios.get(`${apiUrl}/user/profile`,{withCredentials:true});
-            set({data:response.data,loading:false,error:null})
+            set({data:response.data.user,loading:false,error:null})
         }catch{
             set({error:"Une erreure est survenue",loading:false})
         }
