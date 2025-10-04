7575fe372a2567607fa262214b3ccc77947bb5b3
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-04 17:54:24 +0100

    ✨ feat: création du composant ProtectedRoute redirigeant vers /connexion si aucun cookie n’est présent, sinon vers /dashboard

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,21 @@
+import { useProtectedRoutes } from '@/store/userStore'
+import React, { useEffect } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const ProtectedRoute = () => {
+  const { data, loading, fetchData } = useProtectedRoutes()
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  if (loading) return <p>Chargement...</p>
+
+  if (!data) {
+    return <Navigate to="/connexion" replace />
+  }
+
+  return <Outlet />
+}
+
+export default ProtectedRoute
