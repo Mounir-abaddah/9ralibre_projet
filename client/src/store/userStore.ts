78945f5e2ee6f0f95b4b2dc100d4c5259a83eeb2
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-04 17:52:59 +0100

    :sparkles: feat: ajout du store Zustand pour la gestion des données utilisateur protégées et pour la route protege

---
+++ client/src/store/userStore.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { create } from 'zustand';
+
+interface typedata{
+    id: number,
+    nom: string,
+    prenom: string,
+    email: string,
+    role: string,
+    accountVerified: boolean
+}
+
+interface typeAllData{
+    data:typedata[] | null
+    loading:boolean
+    error:string | null
+    fetchData:()=>Promise<void>
+}
+
+export const useProtectedRoutes = create<typeAllData>()((set)=>({
+    data:null,
+    loading:true,
+    error:null,
+    fetchData:async()=>{
+        try{
+            const apiUrl = import.meta.env.VITE_API_URL;
+            const response = await axios.get(`${apiUrl}/user/profile`,{withCredentials:true});
+            set({data:response.data,loading:false,error:null})
+        }catch{
+            set({error:"Une erreure est survenue",loading:false})
+        }
+    }
+}))
