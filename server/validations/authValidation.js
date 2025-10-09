d322d6c91b784d81e3cde316ccd1ebaf315fef8c
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-09 22:09:05 +0100

    :sparkles: feat: réorganisation des schémas de validation et ajout du schéma complet de profil

--- server/validations/authValidation.js
+++ server/validations/authValidation.js
@@ -1,16 +1,7 @@
 const { z } = require("zod");
 
 const registerShema = z.object({
-  nom: z.string()
-    .min(2, "le nom doit contenir minimum 2 caractères")
-    .regex(/^(?!.* {2})(?!.*'')[A-Za-zÀ-ÖØ-öø-ÿ]+([ '][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/, "le nom ne doit contenir que des lettres"),
-  
-  prenom: z.string()
-    .min(2, "le prénom doit contenir minimum 2 caractères")
-    .regex(/^(?!.* {2})(?!.*'')[A-Za-zÀ-ÖØ-öø-ÿ]+([ '][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/, "le prénom ne doit contenir que des lettres"),
-  
   email: z.string().email("Email invalide"),
-
   password: z.string()
     .min(8, "le mot de passe doit contenir minimum 8 caractères")
     .regex(
@@ -35,6 +26,25 @@ const passwordResetShema = z.object({
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
       "le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial"
     )
+});
+
+const completeProfileShema = z.object({
+  nom: z.string()
+    .min(2, "le nom doit contenir minimum 2 caractères")
+    .regex(/^(?!.* {2})(?!.*'')[A-Za-zÀ-ÖØ-öø-ÿ]+([ '][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/, "le nom ne doit contenir que des lettres"),
+  
+  prenom: z.string()
+    .min(2, "le prénom doit contenir minimum 2 caractères")
+    .regex(/^(?!.* {2})(?!.*'')[A-Za-zÀ-ÖØ-öø-ÿ]+([ '][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/, "le prénom ne doit contenir que des lettres"),
+  
+  role:z.enum(['Non renseigné','Etudiant','Etudiante']),
+
+  password: z.string()
+    .min(8, "le mot de passe doit contenir minimum 8 caractères")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      "le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial"
+    )
 })
 
-module.exports = { registerShema ,loginSchema ,messageOUblierSchema,passwordResetShema};
+module.exports = { registerShema ,loginSchema ,messageOUblierSchema,passwordResetShema , completeProfileShema};
