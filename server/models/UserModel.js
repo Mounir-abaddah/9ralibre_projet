53fb9a5cb644dbfb2d7012fbe9e54f27c70d5605
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-09 22:10:05 +0100

    :sparkles: feat: ajout du champ completeProfile dans le schéma utilisateur

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -21,6 +21,10 @@ const UserSchema = new mongoose.Schema({
         type:String,
         default : ""
     },
+    completeProfile:{
+        type:Boolean,
+        default:false
+    },
     accountVerified:{
         type:Boolean,
         default:false
