9b97c558031d764a12b666809d5f227eebabd556
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-12 18:39:37 +0100

    ✨ feat: ajout des interfaces FormDatatype et ErrorType pour la gestion des données de formulaire

---
+++ client/src/components/CompleteProfile/utils/type.ts
@@ -0,0 +1,17 @@
+export interface FormDatatype{
+    nom:string
+    prenom:string
+    role:string
+    niveaux:string
+    password:string
+    confirmPassword:string
+}
+
+export interface ErrorType{
+    nom:string
+    prenom:string
+    role:string
+    niveaux:string
+    password:string
+    confirmPassword:string
+}
