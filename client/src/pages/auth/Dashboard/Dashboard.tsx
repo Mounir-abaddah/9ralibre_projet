b711e5667a910536f6bf9cb4902c80a2e99884ca
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-12 18:39:04 +0100

    ✨ feat: ajout de la gestion du profil complet dans le tableau de bord

--- client/src/pages/auth/Dashboard/Dashboard.tsx
+++ client/src/pages/auth/Dashboard/Dashboard.tsx
@@ -1,10 +1,22 @@
+import CompleteProfile from "@/components/CompleteProfile/CompleteProfile";
 import Layouts from "@/components/Layouts/Layouts"
+import { useProtectedRoutes } from "@/store/userStore"
+import { useEffect } from "react";
 
 const Dashboard = () => {
+  const {data,fetchData} = useProtectedRoutes();  
+  useEffect(()=>{
+    fetchData()
+  },[fetchData])
   document.title = 'Dashboard | 9ralibre'
   return (
       <Layouts>
-        <h1>fddfdfd</h1>
+        {data?.completeProfile
+          ?
+          <h1>ffff</h1>
+          :
+          <CompleteProfile />
+        }
       </Layouts>
   )
 }
