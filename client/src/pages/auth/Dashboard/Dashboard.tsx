da0985b331b3c7b541b96ac4fa5f028ad728377a
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-16 22:35:45 +0100

    ✨ feat: ajout du validation du paramètre `niveaux` et rendu conditionnel entre `CompleteProfile` et contenu principal

--- client/src/pages/auth/Dashboard/Dashboard.tsx
+++ client/src/pages/auth/Dashboard/Dashboard.tsx
@@ -1,19 +1,31 @@
 import CompleteProfile from "@/components/CompleteProfile/CompleteProfile";
 import Layouts from "@/components/Layouts/Layouts"
+import PagesNonTrouver from "@/pages/PagesNonTrouver/PagesNonTrouver";
 import { useProtectedRoutes } from "@/store/userStore"
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const Dashboard = () => {
-  const {data,fetchData} = useProtectedRoutes();  
-  useEffect(()=>{
-    fetchData()
-  },[fetchData])
   document.title = 'Dashboard | 9ralibre'
+  const {niveaux} = useParams()
+  const {data,fetchData} = useProtectedRoutes(); 
+
+    useEffect(()=>{
+    fetchData()
+  },[fetchData]);
+
+  const enumParams = ['1AC','2AC','3AC','TC','1BAC','2BAC'];
+  if(!enumParams.includes(niveaux || '')){
+      return <PagesNonTrouver />
+  }
+
+
+
   return (
       <Layouts>
         {data?.completeProfile
           ?
-          <h1>ffff</h1>
+          <h1>{niveaux}</h1>
           :
           <CompleteProfile />
         }
