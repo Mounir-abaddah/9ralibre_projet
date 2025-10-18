c9a98c51b7f6e3ce3c5992219ac274cc2e28f6f3
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-18 23:21:00 +0100

    :sparkles: feat: ajouter sidebar droit avec calendrier et légende des dates d'examen

---
+++ client/src/components/Layouts/components/right-sidebar/SidebarRight.tsx
@@ -0,0 +1,47 @@
+import { Calendar } from '@/components/ui/calendar';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from '@/components/ui/sidebar';
+import {  Plus, Square } from 'lucide-react';
+import { useState } from 'react';
+import { fr } from "react-day-picker/locale"
+
+const SidebarRight = () => {
+    const [examDates, setExamDates] = useState<Date[] | undefined>([
+      new Date("2025-10-02"),
+      new Date("2025-10-03")
+    ]);
+
+ 
+
+  return (
+    <Sidebar collapsible='none' className='sticky top-0 hidden h-svh border-l lg:flex '>
+      <SidebarHeader className='h-16 border-sidebar-border border-b'>
+        <span>Header sidebar droite</span>
+      </SidebarHeader>
+      <SidebarContent className='w-full overflow-hidden'>
+        <SidebarGroup className='w-full items-center p-2'>
+          <Calendar mode='multiple' locale={fr} selected={examDates} onSelect={setExamDates}/>
+        </SidebarGroup>
+        <SidebarSeparator className='mx-0'/>
+        <SidebarGroup>
+          <SidebarGroupLabel>
+            Légende des dates
+          </SidebarGroupLabel>
+          <span className='text-xs flex items-center gap-2'><Square size={14} fill='#F5F5F5' color='#F5F5F5'/>Date d'aujordhui</span>
+          <span className='text-xs flex items-center gap-2'><Square size={14} fill='#FFC107' color='#FFC107'/>Date de vos examen </span>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton className='flex w-full active:bg-cyan-200 bg-cyan-500 text-white hover:bg-cyan-600 hover:text-white cursor-pointer transition ease all duration-200'>
+              <Plus strokeWidth={2}/>
+              <span className='text-xs'>Ajouter vos Date d'examen </span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
+
+export default SidebarRight
