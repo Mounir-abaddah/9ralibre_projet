f07a40785705f053ff4b9939e25ec792cbc48187
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-18 23:19:44 +0100

    :sparkles: feat: afficher PagesNonTrouver si le type ou le niveau du Dashboard est invalide

--- client/src/pages/auth/Dashboard/Dashboard.tsx
+++ client/src/pages/auth/Dashboard/Dashboard.tsx
@@ -1,5 +1,4 @@
 import CompleteProfile from "@/components/CompleteProfile/CompleteProfile";
-import Layouts from "@/components/Layouts/Layouts"
 import PagesNonTrouver from "@/pages/PagesNonTrouver/PagesNonTrouver";
 import { useProtectedRoutes } from "@/store/userStore"
 import { useEffect } from "react";
@@ -7,7 +6,7 @@ import { useParams } from "react-router-dom";
 
 const Dashboard = () => {
   document.title = 'Dashboard | 9ralibre'
-  const {niveaux} = useParams()
+  const {niveaux,type} = useParams()
   const {data,fetchData} = useProtectedRoutes(); 
 
     useEffect(()=>{
@@ -15,26 +14,27 @@ const Dashboard = () => {
   },[fetchData]);
 
   const enumParams = ['1AC','2AC','3AC','TC','1BAC','2BAC'];
-  if(!enumParams.includes(niveaux || '')){
-      return <PagesNonTrouver />
+  const enumtype = ['Collège','Lycée'];
+
+  if(!enumtype.includes(type || '') || !enumParams.includes(niveaux || '')){
+    return <PagesNonTrouver />
   }
 
   const isEtudiant = data?.role === "Etudiant";
   const borderColor = isEtudiant ? "border-b-sky-400" : "border-pink-400";
-  const title = isEtudiant ? "Mr" : "Mme";
 
 
   return (
-      <Layouts>
+      <div>
         {data?.completeProfile
           ?
           <h1 className="text-xs">
-            Bonjours <span className={`border-b-2 ${borderColor}`}>{title} {data.nom} {data.prenom}</span>
+            Bonjours,<span className={`border-b-2 ${borderColor}`}>{data.nom} {data.prenom}</span>
           </h1>          
           :
           <CompleteProfile />
         }
-      </Layouts>
+      </div>
   )
 }
 
