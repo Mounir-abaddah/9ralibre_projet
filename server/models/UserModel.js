b1f2b73315c454326f20ce2c2099ad2ef0e72c34
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-25 23:38:17 +0100

    :recycle: refactor: simplification de la structure des événements dans le modèle utilisateur

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -48,19 +48,20 @@ const UserSchema = new mongoose.Schema({
             type:Date,
             required:true
         },
-        type:{
-            type:String,
-            required:true
-        },
-        titre:{
-            type:String,
-            required:true
-        },
-        Description:{
-            type:String,
-            required:false
-        }
-        
+        items:[{
+            type:{
+                type:String,
+                required:true
+            },
+            titre:{
+                type:String,
+                required:true
+            },
+            Description:{
+                type:String,
+                required:false
+            }
+        }] 
     }]
 },{
     timestamps:true
