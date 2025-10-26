78a39b8305b85220ca3190907d75e778f6b8177d
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-10-26 21:48:01 +0100

    :sparkles: feat: ajout du composant DrawExcalidraw avec gestion des dessins et intégration des menus

---
+++ client/src/pages/auth/DrawExcalidraw/DrawExcalidraw.tsx
@@ -0,0 +1,44 @@
+import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+import "@excalidraw/excalidraw/index.css";
+import { GitHubDark, Google } from "developer-icons";
+import { useEffect, useState } from "react";
+
+export default function DrawExcalidraw() {
+  const [elements, setElements] = useState(() => {
+    const savedDrawing = localStorage.getItem("drawing");
+    return savedDrawing ? JSON.parse(savedDrawing) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("drawing", JSON.stringify(elements));
+  }, [elements]);
+
+  if (!elements) {
+    return null;
+  }
+
+  return (
+    <div className="size-full">
+      <Excalidraw
+        langCode="fr-FR"
+        initialData={{ elements }}
+        onChange={(updatedElements) => setElements(updatedElements)}
+      >
+        <WelcomeScreen />
+        <MainMenu>
+          <MainMenu.Group title="Excalidraw items">
+            <MainMenu.DefaultItems.Socials />
+            <MainMenu.DefaultItems.Export />
+          </MainMenu.Group>
+          <MainMenu.Group title="9ralibre items">
+          <MainMenu.ItemLink href="https://google.com">
+            <Google size={12}/> Google
+          </MainMenu.ItemLink>
+          <MainMenu.ItemLink href="https://github.com/Mounir-abaddah">
+            <GitHubDark size={12}/> Github
+          </MainMenu.ItemLink>
+          </MainMenu.Group>
+        </MainMenu>
+      </Excalidraw>
+    </div>
+  );
+}
