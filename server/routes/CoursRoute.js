cf4a1d0c3caedfda730ffd07622a443979cd80e3
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-01 18:53:23 +0100

    :sparkles: feat: ajout de la route pour récupérer les cours par niveau

---
+++ server/routes/CoursRoute.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const User = require('../models/UserModel')
+const Niveaux = require('../models/NiveauxModel');
+const Matiere = require('../models/MatiereModel');
+const Cours = require('../models/CoursModel');
+
+router.get("/getCours/:niveauNom", authMiddleware, async (req, res) => {
+  try {
+    const { niveauNom } = req.params;
+
+    const niveau = await Niveaux.findOne({ nom: niveauNom });
+    if (!niveau) {
+      return res.status(404).json({ success: false, message: "Niveau non trouvé" });
+    }
+    const matieres = await Matiere.find({ niveaux: niveau._id });
+    const matiereIds = matieres.map(m => m._id);
+    const cours = await Cours.find({ matiere: { $in: matiereIds } })
+      .populate({
+        path: "matiere",
+        populate: {
+          path: "niveaux",
+          model: "Niveaux"
+        }
+      });
+    return res.json({ success: true, cours });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Erreur lors de la récupération des cours",
+      error: err.message
+    });
+  }
+});
+
+
+
+module.exports = router
