cac78e546bc6ca29a1028d27f2ee8b69a526305d
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-01 18:53:10 +0100

    :sparkles: feat: ajout du modèle Cours avec schéma et validation des champs

---
+++ server/models/CoursModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const CoursShema = new mongoose.Schema({
+    matiere:{
+        type:mongoose.Schema.Types.ObjectId , ref:"matiere"
+    },
+    semstre:{
+        type:String,
+        enum:["Premier Semestre", "Deuxième Semestre"],
+        required:true
+    },
+    type:{
+        type:String, 
+        enum: ["Cours", "Examen National", "Examen Régional"], 
+        required: true 
+    },
+    title:{ 
+        type:String, 
+        required:true 
+    },
+    pdfUrl:{ 
+        type: String, 
+        required: true
+    },
+})
+
+const CoursModels = mongoose.model("Cours", CoursShema)
+
+
+module.exports = CoursModels
