fb85a7bcbd9f49edd6d1102411a07fb3e16f1e13
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-02 22:29:32 +0100

    :sparkles: feat: ajout du composents Matiere qui a des selects matieres et semestre et type

--- client/src/components/Cours/Matiere.tsx
+++ client/src/components/Cours/Matiere.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { XCircle } from "lucide-react"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select"
 
 export interface Coursitems{
@@ -6,10 +6,20 @@ export interface Coursitems{
 }
 export interface CoursMatiere{
     items:Coursitems[]
-    onSelect?:(name:string)=>void
+    onChangeMatiere?: (name: string) => void
+    onChangeSemestre?: (name: string) => void
+    onChangeType?: (name: string) => void
+    selectedMatiere?: string | null
+    selectedSemestre?: string | null
+    selectedType?: string | null
 }
-const Matiere = ({items,onSelect}:CoursMatiere) => {
-    const [active,setActive] = useState<string |null>(null);
+const Matiere = ({  items,
+    onChangeMatiere,
+    onChangeSemestre,
+    onChangeType,
+    selectedMatiere,
+    selectedSemestre,
+    selectedType,}:CoursMatiere) => {
     const bgItems = {
         "Mathématiques":"data-[state=checked]:bg-red-400",
         "Physique et Chimie":"data-[state=checked]:bg-cyan-400",
@@ -22,20 +32,55 @@ const Matiere = ({items,onSelect}:CoursMatiere) => {
         "Éducation Islamique":"data-[state=checked]:bg-blue-400",
     }
   return (
-    <div className="flex h-20 flex-wrap items-center justify-center gap-2">
-        <Select onValueChange={(value)=>{setActive(value);onSelect?.(value)}}>
+    <div className="flex h-20 w-full items-center justify-end gap-2">
+        <div className="flex">
+            {selectedMatiere && (<span className="cursor-pointer" onClick={()=>onChangeMatiere?.("")}><XCircle color="#FF8A8A" strokeWidth={3} size={14}/></span>)}
+            <Select value={selectedMatiere ?? ""} onValueChange={(value)=>{onChangeMatiere?.(value)}}>
             <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Choisissez une matière" />
+                <SelectValue placeholder="Matières" />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
-                <SelectLabel>Matières</SelectLabel>
-                {items.map((item,index)=>(
-                <SelectItem key={index} value={item.name} className={`${bgItems[item.name as keyof typeof bgItems]}`}>{item.name}</SelectItem>
-                ))}
+                    <SelectLabel>Choisissez une matière</SelectLabel>
+                    {items.map((item,index)=>(
+                    <SelectItem key={index} value={item.name} className={`${bgItems[item.name as keyof typeof bgItems]}`}>{item.name}</SelectItem>
+                    ))}
                 </SelectGroup>
             </SelectContent>
         </Select>
+    </div>
+    <div className="flex">
+        {selectedSemestre && (<span className="cursor-pointer" onClick={()=>onChangeSemestre?.("")}><XCircle color="#FF8A8A" strokeWidth={3} size={14}/></span>)}
+        <Select value={selectedSemestre ?? ""} onValueChange={(value)=>{onChangeSemestre?.(value)}}>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Semestre" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                <SelectLabel>Choisissez votre Semestre</SelectLabel>
+                    <SelectItem value="Premier Semestre">Premier Semestre</SelectItem>
+                    <SelectItem value="Deuxième Semestre">Deuxième semestre</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    </div>
+    <div className="flex">
+        {selectedType && (<span className="cursor-pointer" onClick={()=>onChangeType?.("")}><XCircle color="#FF8A8A" strokeWidth={3} size={14}/></span>)} 
+        <Select value={selectedType ?? ""} onValueChange={(value)=>{onChangeType?.(value)}}>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Type" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                <SelectLabel>Selectionner votre type</SelectLabel>
+                    <SelectItem value="Cours">Cours</SelectItem>
+                    <SelectItem value="Exercice">Exercice</SelectItem>
+                    <SelectItem value="Examen National">Examen Nationnaux</SelectItem>
+                    <SelectItem value="Examen Régional">Examen Régionnaux</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    </div>
     </div>
   )
 }
