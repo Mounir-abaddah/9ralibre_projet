2e402ab671d14b44131fae88cacb4a17919c5767
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-03 20:37:28 +0100

    :sparkles: feat: ajout du state filiere et setFiliere dans store

--- client/src/store/useCoursFilter.ts
+++ client/src/store/useCoursFilter.ts
@@ -4,9 +4,11 @@ interface CoursType{
     matiere:string|null,
     semestre:string|null,
     type:string|null,
+    filiere:string|null
     setMatiere:(matiere:string|null)=>void,
     setSemestre:(semestre:string|null)=>void,
     setType:(type:string|null)=>void
+    setFiliere:(filiere:string|null)=>void
     resetAll:()=>void
 }
 
@@ -14,8 +16,10 @@ export const useCoursFilter = create<CoursType>((set)=>({
     matiere:null,
     semestre:null,
     type:null,
+    filiere:null,
     setMatiere:(matiere)=>set({matiere}),
     setSemestre:(semestre)=>set({semestre}),
     setType:(type)=>set({type}),
-    resetAll:()=>set({matiere:null,semestre:null,type:null})
+    setFiliere:(filiere)=>set({filiere}),
+    resetAll:()=>set({matiere:null,semestre:null,type:null,filiere:null})
 }))
