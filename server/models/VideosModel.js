75565b1639b9617bc37050525a36f62e8dad7d1a
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-19 20:53:53 +0100

    :sparkles: feat: creation du VideoModel et definir les champs

---
+++ server/models/VideosModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const VideosShema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        default:""
+    },
+    videoUrl:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        default:""
+    },
+    matiere: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "matiere", 
+        required: true 
+    },
+    niveaux: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Niveaux", 
+        required: true 
+    },
+    filière: { 
+        type: String,
+        required: true 
+    },
+    professeur: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true 
+    },
+    likes: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    ],
+    views: {
+        type: Number,
+        default: 0
+    }
+},{
+    timestamps:true
+})
+
+const VideosModel = mongoose.model("Video",VideosShema,"videos");
+
+module.exports = VideosModel
