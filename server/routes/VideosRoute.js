416e56b95327b80df3d6331c971fe3311b996881
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-22 14:32:38 +0100

    :sparkles: feat: ajout des routes pour gérer les likes et les vues des vidéos

--- server/routes/VideosRoute.js
+++ server/routes/VideosRoute.js
@@ -57,6 +57,58 @@ router.post('/add-videos',authMiddleware,async(req,res)=>{
     }catch(err){
         return res.status(500).send({message:"Une erreure pour ajouter videos",success:false,err})
     }
+});
+
+router.get('/likes/:videoId',authMiddleware,async(req,res)=>{
+    try{
+        const {videoId} = req.params;
+
+        const video = await VideoModel.findById(videoId).populate("likes","nom prenom");
+        if(!video){
+            return res.status(404).send({message:"aucune video est liker",success:false,err:err.message})
+        }
+
+        return res.status(200).json({success:true,likesCount:video.likes.length,likes:video.likes})
+    }catch(err){
+        return res.status(500).send({message:"Une erreure est survenue pour get les videos",success:false,err:err.message})
+    }
+})
+
+router.post('/likes/:videoId',authMiddleware,async(req,res)=>{
+    try{
+        const userId = req.user.userId;
+        const {videoId} = req.params;
+
+        const video = await VideoModel.findById(videoId);
+        if(!video){
+            return res.status(404).send({message:"aucune video est liker",success:false,err:err.message})
+        }
+        if(video.likes.includes(userId)){
+            video.likes.pull(userId);
+            await video.save();
+            return res.json({ liked: false, likes: video.likes.length });
+        }
+        video.likes.push(userId);
+        await video.save();
+        return res.status(200).json({likes:video.likes.length,success:true})
+    }catch(err){
+        return res.status(500).send({message:"Une erreure est survenue pour liker cette videos",success:false,err:err.message})
+    }
+});
+
+router.post('/views/:viewId',authMiddleware,async(req,res)=>{
+    try{
+        const {viewId} = req.params;
+        const video = await VideoModel.findById(viewId);
+        if(!video){
+            return res.status(404).json({ message: "Vidéo non trouvée" });
+        }
+        video.views += 1;
+        await video.save();
+        res.json({ views: video.views });
+    }catch(err){
+        res.status(500).json({ err: err.message });
+    }
 })
 
 
