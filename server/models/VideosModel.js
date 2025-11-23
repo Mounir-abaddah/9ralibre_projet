d6ab25f7edf82b0caadbfb82fa89fa00f441fad3
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-11-23 14:33:48 +0100

    :sparkles: feat: ajout du champ comments dans le modèle Videos pour gérer les commentaires des utilisateurs

--- server/models/VideosModel.js
+++ server/models/VideosModel.js
@@ -42,7 +42,14 @@ const VideosShema = new mongoose.Schema({
     views: {
         type: Number,
         default: 0
-    }
+    },
+    comments: [
+        {
+            user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+            text: { type: String, required: true },
+            createdAt: { type: Date, default: Date.now }
+        }
+    ]
 },{
     timestamps:true
 })
