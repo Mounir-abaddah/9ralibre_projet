de0b8d52af8a4812a21df0bc1a23b5f4c965dc3e
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-12-01 12:11:08 +0100

    :lipstick: refactor: changement du style si le theme est dark

--- client/src/components/Oauth/OAuth.tsx
+++ client/src/components/Oauth/OAuth.tsx
@@ -11,19 +11,19 @@ const OAuth = ({ text_1, text_2 }: OauthType) => {
   }
   return (
     <div className="w-full">
-      <div className="flex w-full justify-around lg:flex-row md:flex-row flex-col-reverse gap-3 ">
-        <div onClick={handleClick} className="flex cursor-pointer w-full items-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:bg-slate-50 hover:inset-shadow-md">
+      <div className="flex w-full flex-col-reverse justify-around gap-3 md:flex-row lg:flex-row ">
+        <div onClick={handleClick} className="hover:inset-shadow-md flex w-full cursor-pointer items-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:bg-slate-50 dark:border-gray-300">
           <Google size={19}/>
           <hr className="h-6 rounded-md border-1" />
           <p>{text_1}</p>
         </div>
-        <div className="flex cursor-pointer bg-black text-white w-full items-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:inset-shadow-md">
+        <div className="hover:inset-shadow-md flex w-full cursor-pointer items-center gap-2 rounded-md border bg-black p-2 font-medium text-white transition-all duration-300">
           <Microsoft size={19} />
           <hr className="h-6 rounded-md border-1" />
           <p>{text_2}</p>
         </div>
       </div>
-      <div className="flex w-full items-center justify-around gap-4 mt-3">
+      <div className="mt-3 flex w-full items-center justify-around gap-4">
         <hr className="w-full" />
         <h6 className="font-semibold">OU</h6>
         <hr className="w-full" />
