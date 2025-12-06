b709bbe303a3823678b65fcde10156e40f1a4adf
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-12-06 20:29:26 +0100

    :sparkles: feat: ajout du Shema viewrs pour savoir le utilisateur qui a regarder la video

--- server/models/VideosModel.js
+++ server/models/VideosModel.js
@@ -43,13 +43,17 @@ const VideosShema = new mongoose.Schema({
         type: Number,
         default: 0
     },
+    viewers: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User' 
+    }],
     comments: [
         {
             user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
             text: { type: String, required: true },
             createdAt: { type: Date, default: Date.now }
         }
-    ]
+    ],
 },{
     timestamps:true
 })
