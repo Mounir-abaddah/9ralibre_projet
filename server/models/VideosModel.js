c17052ca4375f604ca843c5a655863ff73694863
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-12-08 22:21:33 +0100

    :sparkles: feat: ajout du champ replies dans champ comments pour que utilisateur repoondre un commentaire et liker

--- server/models/VideosModel.js
+++ server/models/VideosModel.js
@@ -51,7 +51,13 @@ const VideosShema = new mongoose.Schema({
         {
             user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
             text: { type: String, required: true },
-            createdAt: { type: Date, default: Date.now }
+            createdAt: { type: Date, default: Date.now },
+            replies:[{
+                user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+                text: { type: String, required: true },
+                createdAt: { type: Date, default: Date.now },
+        }],
+        likes:[ {type:mongoose.Schema.Types.ObjectId , ref:"User" } ]
         }
     ],
 },{
