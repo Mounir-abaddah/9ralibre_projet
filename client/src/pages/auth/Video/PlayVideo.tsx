a913b68fd3558886f85c17d1b08c1dced58bf8db
Mounir-abaddah <mounirabaddah6@gmail.com>
2025-12-24 23:02:04 +0100

    :sparkles: feat: ajout de la fonctionnalité de suivi du professeur et amélioration de l'affichage des vidéos

--- client/src/pages/auth/Video/PlayVideo.tsx
+++ client/src/pages/auth/Video/PlayVideo.tsx
@@ -9,6 +9,7 @@ import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
 import { fr } from "date-fns/locale";
 import Comments from "@/components/Videos/Comments";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import RelatedVideo from "@/components/Videos/RelatedVideo";
 
 const PlayVideo = () => {
     const apiUrl = import.meta.env.VITE_API_URL;
@@ -16,10 +17,13 @@ const PlayVideo = () => {
     const [videos,setVideos]=useState<TypeVideos | null>(null);
     const [isLike,setisLike]= useState(false);
 
+    const [likeProfesseur,setLikeProfesseur]=useState(false)
+
     const getVideos = async()=>{
         const res = await axios.get(`${apiUrl}/videos/get-videos-id/${videoId}`,{withCredentials:true});
         setVideos(res.data.videos)
         setisLike(res.data.isLikes)
+        setLikeProfesseur(res.data.isFollowProfesseur)
     };
     
     useEffect(()=>{
@@ -32,18 +36,25 @@ const PlayVideo = () => {
         await getVideos();
     }
 
+    const handleFollowProfesseur = async(profId:string)=>{
+        const res = await axios.post(`${apiUrl}/user/follow/${profId}`,{},{withCredentials:true});
+        setLikeProfesseur(res.data.following)
+        await getVideos();
+    }
+
     if(!videos) return <div>Chargement ....</div>
     document.title = `Videos - ${videos?.title} | 9ralibre`
 return (
-    <div className="flex w-full p-4">
-        <div className="flex max-w-full flex-col space-y-4">
+    <div className="flex w-full flex-col gap-6 p-4 lg:flex-row">
+        <div className="flex w-full flex-col space-y-4">
             {/*****VIDEOURL REACT PLAYER*****/}
             <div>
                 <ReactPlayer 
                     src={videos.videoUrl} 
-                    width={1000} 
-                    height={400} 
-                    controls 
+                    controls
+                    width={'100%'}
+                    height={400}
+                    className="rounded-md"
                 />
             </div>
             {/*****TITRE *****/}
@@ -51,7 +62,7 @@ return (
                 <h1 className="text-2xl font-bold">{videos.title}</h1>
             </div>
             {/*****Professeur(Follow,Followers) , LIKES ET SHARE ET SAVE *****/}
-            <div className="flex w-full justify-between">
+            <div className="flex w-full flex-wrap justify-between space-y-4 md:space-y-0 lg:space-y-0">
                 {/*****Professeur(Follow,Followers) *****/}
                 <div className="flex gap-2">
                     <img src={videos.professeur.image} alt="image_de_professeur" width={40} className="rounded-full"/>
@@ -60,18 +71,20 @@ return (
                         <span className="text-xs text-gray-400">{videos.professeur.followers.length}  d’abonnés</span>
                     </div>
                     <div>
-                        <Button className="cursor-pointer bg-amber-500 hover:bg-amber-600">S'abonner</Button>
+                        <Button onClick={()=>handleFollowProfesseur(videos.professeur._id)} className="cursor-pointer bg-amber-500 hover:bg-amber-600">{likeProfesseur ? 'Déjà abonné(e)' : 'S’abonner'}</Button>
                     </div>
                 </div>
                 {/*****LIKES ET SHARE ET SAVE *****/}
                 <div className="flex gap-2">
                     <Button className={`flex cursor-pointer items-center gap-1 rounded-md  text-xs transition-all duration-200 hover:bg-gray-100 active:scale-95 ${isLike ? 'text-red-500' : ''}`} onClick={handleLikes}><Heart color={isLike ? '#FF2E2E' : '#000'} fill={isLike ? '#FF2E2E' : '#fff'}/>{videos.likes.length}</Button>
-                    <Button className="cursor-pointer"><Share />Partager</Button>
+                    <Button className="hidden cursor-pointer md:flex lg:flex"><Share />Partager</Button>
+                    <Button variant="outline" className="hidden cursor-pointer md:flex lg:flex"><Bookmark />Enregistrer</Button>
                     <Popover>
                         <PopoverTrigger asChild>
                             <Button variant="outline" className="cursor-pointer"><EllipsisVertical /></Button>
                         </PopoverTrigger>
                         <PopoverContent className="w-42 space-y-2">
+                            <Button variant="outline" className="flex w-full cursor-pointer md:hidden lg:hidden"><Share />Partager</Button>
                             <Button variant="outline" className="w-full cursor-pointer"><Bookmark />Enregistrer</Button>
                             <Button variant="outline" className="w-full cursor-pointer"><Flag />Signaler</Button>
                         </PopoverContent>
@@ -87,7 +100,8 @@ return (
             <Comments videos={videos} getVideos={getVideos}/>
         </div>
         {/******************RELATED VIDEOS******************/}
-        <div className="w-full">
+        <div className="w-full md:w-full lg:w-[40%]">
+            <RelatedVideo />
         </div>
     </div>
 )
