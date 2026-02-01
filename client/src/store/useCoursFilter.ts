b8d5842a5e6f05c3730cd8e800605fcfab90592d
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-02-01 15:00:17 +0100

    :sparkles: feat: ajout de la propriété search et de la méthode setSearch dans le filtre de cours

--- client/src/store/useCoursFilter.ts
+++ client/src/store/useCoursFilter.ts
@@ -1,14 +1,16 @@
 import { create } from "zustand";
 
-interface CoursType{
+export interface CoursType{
     matiere:string|null,
     semestre:string|null,
     type:string|null,
     filiere:string|null
+    search?:string|null
     setMatiere:(matiere:string|null)=>void,
     setSemestre:(semestre:string|null)=>void,
     setType:(type:string|null)=>void
     setFiliere:(filiere:string|null)=>void
+    setSearch:(title:string|null)=>void
     resetAll:()=>void
 }
 
@@ -17,9 +19,11 @@ export const useCoursFilter = create<CoursType>((set)=>({
     semestre:null,
     type:null,
     filiere:null,
+    search:null,
     setMatiere:(matiere)=>set({matiere}),
     setSemestre:(semestre)=>set({semestre}),
     setType:(type)=>set({type}),
     setFiliere:(filiere)=>set({filiere}),
-    resetAll:()=>set({matiere:null,semestre:null,type:null,filiere:null})
+    setSearch:(search)=>set({search}),
+    resetAll:()=>set({matiere:null,semestre:null,type:null,filiere:null,search:null})
 }))
