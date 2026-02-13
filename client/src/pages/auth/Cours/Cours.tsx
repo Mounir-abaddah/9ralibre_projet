98b2d9d2d9c0b1010a900baf5910caad999146bc
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-02-13 15:38:32 +0100

    :recycle: refactor: changement du path du Professeur

--- client/src/pages/auth/Cours/Cours.tsx
+++ client/src/pages/auth/Cours/Cours.tsx
@@ -218,7 +218,7 @@ const Cours = () => {
             <div className={`absolute top-0 left-0 flex items-center gap-2 rounded-br-2xl ${bgItems[item.matiere.nom as keyof typeof bgItems]} px-3 py-1 text-xs font-medium text-white shadow-sm`}>
               <span className="flex items-center gap-1">
                 <IconeProfesseur />
-                Professeur : <Link to={`/Professeur/${encodeURIComponent(`${item.professeur.nom} ${item.professeur.prenom}`)}`} className="text-xs font-semibold hover:underline">{`${item.professeur.nom} ${item.professeur.prenom}`.toUpperCase()}</Link>
+                Professeur : <Link to={`/Profile/${encodeURIComponent(`${item.professeur.nom}-${item.professeur.prenom}`)}`} className="text-xs font-semibold hover:underline">{`${item.professeur.nom} ${item.professeur.prenom}`.toUpperCase()}</Link>
               </span>
             </div>
             <CardHeader className='mt-2 flex items-center justify-between'>
