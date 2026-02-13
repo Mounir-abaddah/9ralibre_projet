bdb6b2de017b1f7a9c161c1475ac6e2c4896a7d2
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-02-13 15:23:16 +0100

    :sparkles: feat: ajout du configuration socket et implementer cors  et initialisation du socket

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const passport = require('passport')
 const path = require('path')
 const cookieParser = require('cookie-parser');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 
 require('./config/passport')
 
@@ -25,14 +28,44 @@ const userRouter = require('./routes/UserRoute');
 const coursRouter = require('./routes/CoursRoute');
 const videosRouter = require('./routes/VideosRoute');
 const quizRouter = require('./routes/QuizRoute');
+const messagerieRouter = require('./routes/MessagerieRoute')
 
 app.use('/auth',authRouter)
 app.use('/user',userRouter)
 app.use('/cours',coursRouter);
 app.use('/videos',videosRouter)
 app.use('/quiz',quizRouter);
+app.use('/messagerie',messagerieRouter);
+
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer,{
+    cors:({
+        origin:process.env.FRONTEND_URL,
+        credentials:true
+    })
+})
+
+
+io.on("connection", (socket) => {
+    console.log("Un utilisateur est connecté : ", socket.id);
+
+    socket.on("joinRoom", (conversationId) => {
+        socket.join(conversationId);
+        console.log(`Socket ${socket.id} rejoint room ${conversationId}`);
+    });
+
+    socket.on("sendMessage", ({ conversationId, message }) => {
+        socket.to(conversationId).emit("receiveMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Utilisateur déconnecté : ", socket.id);
+    });
+});
 
 const port = process.env.PORT;
-app.listen(port,()=>{
+httpServer.listen(port,()=>{
     console.log(`Server is listening on PORT ${port}`);
 })
