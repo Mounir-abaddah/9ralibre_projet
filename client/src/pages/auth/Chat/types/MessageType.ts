6b77d858833d79060f9034e9d0019af2fbdf1e17
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-02-20 14:08:59 +0000

    :fire: clear: supprimation des pages parce que j'ai rien compris donc a supprimer

--- client/src/pages/auth/Chat/types/MessageType.ts
+++
@@ -1,8 +0,0 @@
-export interface MessageType {
-  _id: string
-  conversationId: string
-  sender: string
-  text: string
-  createdAt: string
-  updatedAt: string
-}
--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ const cors = require('cors');
 const passport = require('passport')
 const path = require('path')
 const cookieParser = require('cookie-parser');
-const { createServer } = require("http");
-const { Server } = require("socket.io");
 
 
 require('./config/passport')
@@ -27,47 +25,14 @@ const authRouter = require('./routes/AuthRoute');
 const userRouter = require('./routes/UserRoute');
 const coursRouter = require('./routes/CoursRoute');
 const videosRouter = require('./routes/VideosRoute');
-const quizRouter = require('./routes/QuizRoute');
-const messagerieRouter = require('./routes/MessagerieRoute');
-const enregistrerRouter = require('./routes/EnregistrerRoute')
 
 app.use('/auth',authRouter)
 app.use('/user',userRouter)
 app.use('/cours',coursRouter);
 app.use('/videos',videosRouter)
-app.use('/quiz',quizRouter);
-app.use('/messagerie',messagerieRouter);
-app.use('/save',enregistrerRouter)
 
-
-const httpServer = createServer(app);
-
-const io = new Server(httpServer,{
-    cors:({
-        origin:process.env.FRONTEND_URL,
-        credentials:true
-    })
-})
-
-
-io.on("connection", (socket) => {
-    console.log("Un utilisateur est connecté : ", socket.id);
-
-    socket.on("joinRoom", (conversationId) => {
-        socket.join(conversationId);
-        console.log(`Socket ${socket.id} rejoint room ${conversationId}`);
-    });
-
-    socket.on("sendMessage", ({ conversationId, message }) => {
-        socket.to(conversationId).emit("receiveMessage", message);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("Utilisateur déconnecté : ", socket.id);
-    });
-});
 
 const port = process.env.PORT;
-httpServer.listen(port,()=>{
+app.listen(port,()=>{
     console.log(`Server is listening on PORT ${port}`);
 })
