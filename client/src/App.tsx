8481fd63f300c62ce55b26b731eff4ab7d0e4dd1
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-02-21 14:23:30 +0000

    :fire: clear: supprimation des fichier pour initialiser le project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,11 +17,8 @@ import Settings from './pages/auth/Settings/Settings';
 import { ThemeProvider } from './context/ThemeContext';
 import Videos from './pages/auth/Video/Video';
 import PlayVideo from './pages/auth/Video/PlayVideo';
-import Quiz from './pages/auth/Quiz/Quiz';
-import Quiz_id from './pages/auth/Quiz/Quiz_id';
 import Histoire from './pages/auth/Histoire/Histoire';
 import Profile from './pages/auth/Profile/Profile';
-import Chat from './pages/auth/Chat/Chat';
 
 const App = () => {
   return (
@@ -44,10 +41,7 @@ const App = () => {
             <Route path='/Cours/:niveaux' element={<Layouts><Cours /></Layouts>} />
             <Route path='/Videos/:niveaux' element={<Layouts><Videos /></Layouts>} />
             <Route path='/Videos/:niveaux/:videoId' element={<Layouts><PlayVideo /></Layouts>} />
-            <Route path='/Quiz/:niveaux' element={<Layouts><Quiz /></Layouts>} />
-            <Route path='/Quiz/:niveaux/:quizId' element={<Layouts><Quiz_id /></Layouts>} />
             <Route path='/Histoire/:niveaux' element={<Layouts><Histoire /></Layouts>} />
-            <Route path='/Chat/:Conversationid' element={<Layouts><Chat /></Layouts>} />
             <Route path='/Paramètre/:niveaux' element={<Layouts><Settings /></Layouts>} />
         </Route>
         <Route />
