84811172174cbb689bd1926d3a4c0fb43e72c836
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-02-27 23:29:34 +0000

    :sparkles: feat: ajout du composant Chat avec interface de messagerie

---
+++ client/src/pages/auth/Chat/Chat.tsx
@@ -0,0 +1,24 @@
+import AsideChat from "@/components/Chat/AsideChat"
+import Work_Chat from "@/assets/images/Work chat-cuate.png"
+const Chat = () => {
+  return (
+    <div className="flex w-full">
+        <div className="w-2xl">
+            <AsideChat />
+        </div>
+        <div className="flex w-full flex-col items-center justify-center gap-4">
+            <img src={Work_Chat} alt="chat_img" width={400} />
+            <div className="text-center">
+            <h2 className="text-2xl font-semibold text-gray-800">
+                Bienvenue sur votre espace de messagerie
+            </h2>
+            <p className="mt-2 text-gray-500">
+                Échangez facilement avec vos <span className="font-medium text-amber-600">professeurs</span> ou vos <span className="font-medium text-amber-600">camarades</span> — posez vos questions, partagez vos ressources et collaborez en toute simplicité.
+            </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Chat
