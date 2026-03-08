87279cdfd6526490721877d0a06511ebef8f89b1
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-03-08 16:12:18 +0000

    :sparkles: feat: ajout du  Professeur dans le role du inteface typedata

--- client/src/store/userStore.ts
+++ client/src/store/userStore.ts
@@ -7,7 +7,7 @@ export interface typedata{
     nom: string,
     prenom: string,
     email: string,
-    role: 'Non renseigné'| 'Etudiant' |'Etudiante',
+    role: 'Non renseigné'| 'Etudiant' |'Etudiante' | 'Professeur',
     niveaux: '1AC' | '2AC' | '3AC' | 'TC'| '1BAC' | '2BAC',
     image:string
     accountVerified: boolean
