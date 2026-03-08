6b1b4a12b20ab51a8a21ad839c25653da4a9e38c
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-03-08 16:18:50 +0000

    :fire: clear: supprimation des champ password et comfirmation du password dans les validataions

--- client/src/components/CompleteProfile/utils/validation.ts
+++ client/src/components/CompleteProfile/utils/validation.ts
@@ -8,15 +8,12 @@ export const validateForm = (
   formData: FormDatatype,
   setErrors: (e: ErrorType) => void,
   step: number,
-  data: { provider?: string } | null 
 ): boolean => {
   const newErrors: ErrorType = {
     nom: "",
     prenom: "",
     role: "",
     niveaux:"",
-    password: "",
-    confirmPassword: "",
   };
 
    let valid = true;
@@ -44,26 +41,6 @@ export const validateForm = (
       valid = false;
     }
   }
-
-  if (data?.provider === "google") {
-    if (
-      !formData.password.trim() ||
-      !/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(formData.password)
-    ) {
-      newErrors.password =
-        "Minimum 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial";
-      valid = false;
-    }
-
-    if (
-      formData.password !== formData.confirmPassword ||
-      !formData.confirmPassword
-    ) {
-      newErrors.confirmPassword = "Les mots de passe ne correspondent pas";
-      valid = false;
-    }
-  }
-
   setErrors(newErrors);
   return valid;
 };
