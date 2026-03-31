d8bd354a9309eff651a7706c9ee44f84d0c8944c
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-03-31 20:52:29 +0100

    :sparkles: feat: ajout de la page ProfQuiz pour gérer et afficher les quiz avec options de suppression

---
+++ client/src/pages/auth/Professeur/Prof-quiz/ProfQuiz.tsx
@@ -0,0 +1,189 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { Trash, Users } from "lucide-react";
+import Pagination from "@/components/Pagination/Pagination";
+import type { QuizProf } from "../../Quiz/types/QuizType";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const ProfQuiz = () => {
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const [quiz, setQuiz] = useState<QuizProf[]>([]);
+    const [page, setPage] = useState(1);
+    const [totalQuiz, setTotalQuiz] = useState(0);
+    const limit = 8;
+
+    const options:Intl.DateTimeFormatOptions = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    const getQuiz = async (pageNumber = 1) => {
+        const res = await axios.get(`${apiUrl}/prof/get-quiz?page=${pageNumber}&limit=${limit}`,{ withCredentials: true });
+        setQuiz(res.data.quiz);
+        setTotalQuiz(res.data.totalQuiz);
+    };
+
+    const handleDelete = async(quizId:string)=>{
+        await axios.delete(`${apiUrl}/prof/delete-quiz/${quizId}`,{withCredentials:true})
+        await getQuiz(page);
+        toast.success("Cours supprimé avec succès ✅");
+    }
+
+    useEffect(() => {
+        getQuiz(page);
+    }, [page]);
+
+    return (
+        <div className="space-y-6">
+        {/* HEADER */}
+        <div className="flex items-center justify-between">
+            <div>
+                <h2 className="text-2xl font-bold">Mes Quiz</h2>
+                <span className="rounded-full px-4 py-1 text-sm text-white">
+                    {totalQuiz} Quiz disponible
+                </span>
+            </div>
+            <Link target="_blank" to={'/prof/add/quiz/questions'}>
+                <Button variant={'outline'} className="cursor-pointer">
+                    Ajouter un Quiz
+                </Button>
+            </Link>
+            
+        </div>
+
+        {/* TABLE */}
+        <div className="overflow-hidden rounded-xl border shadow-sm">
+            <Table>
+            <TableHeader className="bg-gray-100 dark:bg-slate-800">
+                <TableRow>
+                <TableHead>Titre</TableHead>
+                <TableHead>Questions</TableHead>
+                <TableHead>Filière</TableHead>
+                <TableHead>Matiere</TableHead>
+                <TableHead>
+                    <Tooltip>
+                        <TooltipTrigger >
+                            Étudiants
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Étudiants passer le quiz</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
+                </TableRow>
+            </TableHeader>
+
+            <TableBody>
+                {quiz.map((q) => (
+                <TableRow key={q._id} className="transition hover:bg-gray-50 dark:hover:bg-slate-800">
+                    <TableCell className="font-semibold">
+                        {q.text}
+                    </TableCell>
+
+                    <TableCell>
+                        {q.questions.length}
+                    </TableCell>
+
+                    <TableCell>
+                    <span className="rounded bg-blue-100 px-2 py-1 text-xs text-blue-600">
+                        {q.filiere}
+                    </span>
+                    </TableCell>
+
+                    <TableCell>
+                    <span className="rounded bg-amber-100 px-2 py-1 text-xs text-amber-600">
+                        {q.matiere.nom}
+                    </span>
+                    </TableCell>
+
+                    <TableCell>
+                    <div className="flex items-center gap-1">
+                        <Users size={16} />
+                        {q.participants?.length || 0}
+                    </div>
+                    </TableCell>
+
+                    <TableCell>
+                    {new Date(q.createdAt).toLocaleDateString("fr-FR",options)}
+                    </TableCell>
+
+                    <TableCell className="flex justify-end gap-2 text-right">
+                        <AlertDialog>
+                            <AlertDialogTrigger asChild>
+                            <Button
+                                variant="outline"
+                                className="rounded-lg p-2 text-red-500 hover:bg-red-100"
+                            >
+                                <Trash size={16} />
+                            </Button>
+                            </AlertDialogTrigger>
+
+                            <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>
+                                    Confirmer la suppression
+                                </AlertDialogTitle>
+
+                                <AlertDialogDescription>
+                                Voulez-vous vraiment supprimer ce quiz :
+                                <span className="font-semibold"> {q.text} </span> ?
+                                Cette action est irréversible.
+                                </AlertDialogDescription>
+                            </AlertDialogHeader>
+
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Annuler</AlertDialogCancel>
+
+                                <AlertDialogAction
+                                onClick={() => handleDelete(q._id)}
+                                className="bg-red-600 hover:bg-red-700"
+                                >
+                                    Oui, supprimer
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                            </AlertDialogContent>
+                        </AlertDialog>
+                        </TableCell>
+                </TableRow>
+                ))}
+            </TableBody>
+            </Table>
+        </div>
+        {/* PAGINATION */}
+        <Pagination
+            totalItems={totalQuiz}
+            itemsPerPage={limit}
+            currentPage={page}
+            onPageChange={setPage}
+        />
+        </div>
+    );
+};
+
+export default ProfQuiz;
