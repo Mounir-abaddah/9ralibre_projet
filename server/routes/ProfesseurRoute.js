2095ab3ea05b35d3872b04fd3920b5b3d770752d
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-01 13:46:23 +0100

    :sparkles: feat: ajout des routes pour la gestion des conversations et des messages

--- server/routes/ProfesseurRoute.js
+++ server/routes/ProfesseurRoute.js
@@ -9,7 +9,7 @@ const Videos = require('../models/VideosModel');
 const Conversation = require('../models/ConversationModel');
 const Matiere = require('../models/MatiereModel');
 const Niveaux = require('../models/NiveauxModel');
-
+const Message = require('../models/MessagesModel');
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
@@ -593,6 +593,67 @@ router.delete('/delete-quiz/:quizId', authMiddlewares, profMiddleware, async (re
     console.error(error);
     res.status(500).json({success: false,message: "Erreur serveur"});
   }
+});
+
+
+
+router.get('/my-conversation', authMiddlewares,profMiddleware, async (req, res) => {
+    const userId = req.user.userId;
+
+    const conversations = await Conversation.find({
+      members: userId
+    })
+    .populate("members", "nom prenom role image")
+    .populate("lastMessage", "text sender")
+    .sort({createdAt:-1});
+
+    const conversationsWithUnread = await Promise.all(
+      conversations.map(async (conv) => {
+        const unreadCount = await Message.countDocuments({
+          conversationId: conv._id,
+          readBy: { $ne: userId },
+          sender: { $ne: userId }
+        });
+
+        return {
+          ...conv.toObject(),
+          unreadCount
+        };
+      })
+    );
+
+    res.json(conversationsWithUnread);
+});
+
+router.get('/my-conversation/:conversationId',authMiddlewares,profMiddleware,async(req,res)=>{
+    const conversationId = req.params.conversationId
+    const conversation = await Conversation.findById(conversationId).populate("members","nom prenom role image")
+    .populate("lastMessage","text sender")
+    res.json(conversation)
+});
+
+router.get('/get-messages/:conversationId',authMiddlewares,profMiddleware,async(req,res)=>{
+    const conversationId = req.params.conversationId;
+    const messages = await Message.find({
+        conversationId:conversationId
+    }).populate("sender", "nom prenom image")
+    .sort({ createdAt: 1 });
+    res.json(messages)
+});
+
+router.post('/messages',authMiddlewares,profMiddleware,async(req,res)=>{
+    const userId = req.user.userId;
+    const {conversationId,text} = req.body;
+    const messages = await Message.create({
+        conversationId,
+        sender:userId,
+        text,
+        readBy:[req.user.userId]
+    })
+    await Conversation.findByIdAndUpdate(conversationId,{
+        lastMessage:messages._id
+    })
+    res.status(201).json(messages)
 });
 
 
