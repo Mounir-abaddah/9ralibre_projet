bf399d50b0bea32e7d8b202e4352309652c6a0f1
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-08 13:19:40 +0100

    :recycle: refactor: Masquer le composents AsideChat si ya pas conversation

--- client/src/pages/auth/Chat/Chat.tsx
+++ client/src/pages/auth/Chat/Chat.tsx
@@ -1,11 +1,25 @@
 import AsideChat from "@/components/Chat/AsideChat"
 import Work_Chat from "@/assets/images/Work chat-cuate.png"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import type { typeChat } from "./types/ChatType";
 const Chat = () => {
-  return (
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const [conversation, setConversation] = useState<typeChat[]>([]);
+    const fetchConversations = async () => {
+        const res = await axios.get(`${apiUrl}/chat/my-conversation`, { withCredentials: true });
+        setConversation(res.data);
+    };
+    useEffect(() => {
+        fetchConversations();
+    }, [apiUrl]);
+return (
     <div className="flex w-full">
-        <div className="w-2xl">
-            <AsideChat />
-        </div>
+        {conversation.length > 0 && (
+            <div className="w-2xl">
+                <AsideChat />
+            </div>
+        )}
         <div className="flex w-full flex-col items-center justify-center gap-4">
             <img src={Work_Chat} alt="chat_img" width={400} />
             <div className="text-center">
@@ -18,7 +32,7 @@ const Chat = () => {
             </div>
         </div>
     </div>
-  )
+)
 }
 
 export default Chat
