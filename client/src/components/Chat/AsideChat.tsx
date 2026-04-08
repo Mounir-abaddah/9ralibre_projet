3a160df5cf9598cee38c616fdd76e3b8760a77e6
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-08 13:14:54 +0100

    :lipstick: feat: ameliorer le designe du AsideChat

--- client/src/components/Chat/AsideChat.tsx
+++ client/src/components/Chat/AsideChat.tsx
@@ -56,62 +56,66 @@ const filteredConversations = conversation.filter((conv) => {
 });
 
   return (
-    <div className="space-y-3 p-3">
-      <Input type="text" placeholder="Rechercher dans les messageries" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-      {filteredConversations.map((conv) => {
-        const otherUser = conv.members.find((member) => member._id !== data?.id);
-        const isLastMessageMine = conv.lastMessage?.sender === data?.id;
-        const isOnline = otherUser ? onlineUsers.includes(otherUser._id ?? "") : false;
-        return (
-          <div
-            key={conv._id}
-            onClick={() => navigate(`/Chat/start/${conv._id}`)}
-            className={`flex cursor-pointer items-center gap-2 rounded-md border p-4 transition
-              ${chatId === conv._id ? "border-cyan-400 bg-cyan-100 text-black" : "hover:bg-gray-50 hover:text-black"}`}
-          >
-            {otherUser ? (
-              <>
-                <div className="relative">
-                  <Avatar className="h-12 w-12">
-                    <AvatarImage src={`${apiUrl}/uploads/images/${otherUser?._id}/${otherUser?.image}`} alt="utilisateur"/>
-                    <AvatarFallback className={`rounded font-bold tracking-wide uppercase shadow-sm ${
-                      otherUser?.role === "Etudiant" ? "bg-sky-500 text-white" :
-                      otherUser?.role === "Etudiante" ? "bg-pink-500 text-white" :
-                      "bg-gray-200 text-gray-700"}`}
-                    >
-                      {otherUser?.nom[0]}{otherUser?.prenom[0]}
-                    </AvatarFallback>
-                  </Avatar>
-                  <span
-                    className={`absolute right-0 bottom-0 h-3 w-3 rounded-full border-2 border-white ${
-                      isOnline ? "bg-green-500" : "bg-gray-400"
-                    }`}
-                  />
-                </div>
-                <div className="flex w-full flex-col overflow-hidden">
-                  <span className="text-base font-medium">{otherUser.nom} {otherUser.prenom}</span>
-                  <div className="flex w-full items-center justify-between">
-                    <span className="line-clamp-1 text-sm text-gray-500">
-                      <span className="font-medium text-gray-700">
-                        {isLastMessageMine ? "Vous : " : `${otherUser.nom} : `}
-                      </span>
-                      {conv.lastMessage?.text}
-                    </span>
-                    {/* ✅ Badge unread */}
-                    {conv.unreadCount > 0 && (
-                      <span className="rounded-full bg-red-500 px-2 py-0.5 text-xs text-white">
-                        {conv.unreadCount}
-                      </span>
-                    )}
-                  </div>
-                </div>
-              </>
-            ) : (
-              <span className="text-gray-400">Utilisateur inconnu</span>
-            )}
-          </div>
-        );
-      })}
+    <div className="space-y-3 p-2">
+      {conversation.length > 0 && (
+        <div className="space-y-3">
+          <Input type="text" placeholder="Rechercher dans les messageries" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+          {filteredConversations.map((conv) => {
+            const otherUser = conv.members.find((member) => member._id !== data?.id);
+            const isLastMessageMine = conv.lastMessage?.sender === data?.id;
+            const isOnline = otherUser ? onlineUsers.includes(otherUser._id ?? "") : false;
+            return (
+              <div
+                key={conv._id}
+                onClick={() => navigate(`/Chat/start/${conv._id}`)}
+                className={`flex cursor-pointer items-center gap-2 rounded-md border p-4 transition
+                  ${chatId === conv._id ? "border-cyan-400 bg-cyan-100 text-black" : "hover:bg-gray-50 hover:text-black"}`}
+              >
+                {otherUser ? (
+                  <>
+                    <div className="relative">
+                      <Avatar className="h-12 w-12">
+                        <AvatarImage src={`${apiUrl}/uploads/images/${otherUser?._id}/${otherUser?.image}`} alt="utilisateur"/>
+                        <AvatarFallback className={`rounded font-bold tracking-wide uppercase shadow-sm ${
+                          otherUser?.role === "Etudiant" ? "bg-sky-500 text-white" :
+                          otherUser?.role === "Etudiante" ? "bg-pink-500 text-white" :
+                          "bg-gray-200 text-gray-700"}`}
+                        >
+                          {otherUser?.nom[0]}{otherUser?.prenom[0]}
+                        </AvatarFallback>
+                      </Avatar>
+                      <span
+                        className={`absolute right-0 bottom-0 h-3 w-3 rounded-full border-2 border-white ${
+                          isOnline ? "bg-green-500" : "bg-gray-400"
+                        }`}
+                      />
+                    </div>
+                    <div className="flex w-full flex-col overflow-hidden">
+                      <span className="text-base font-medium">{otherUser.nom} {otherUser.prenom}</span>
+                      <div className="flex w-full items-center justify-between">
+                        <span className="line-clamp-1 text-sm text-gray-500">
+                          <span className="font-medium text-gray-700">
+                            {isLastMessageMine ? "Vous : " : `${otherUser.nom} : `}
+                          </span>
+                          {conv.lastMessage?.text}
+                        </span>
+                        {/* ✅ Badge unread */}
+                        {conv.unreadCount > 0 && (
+                          <span className="rounded-full bg-red-500 px-2 py-0.5 text-xs text-white">
+                            {conv.unreadCount}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <span className="text-gray-400">Utilisateur inconnu</span>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
