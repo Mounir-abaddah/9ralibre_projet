a79ff4b365b841d64ec693f44edd3ca76dadd1eb
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-08 16:10:56 +0100

    :lipstick: feat: améliorer le design du composant RadioGroupItem en ajustant la position et la taille de l'icône

--- client/src/components/ui/radio-group.tsx
+++ client/src/components/ui/radio-group.tsx
@@ -34,7 +34,7 @@ function RadioGroupItem({
         data-slot="radio-group-indicator"
         className="relative flex items-center justify-center"
       >
-        <CircleIcon className="absolute top-1/2 left-1/2 size-2 -translate-x-1/2 -translate-y-1/2 fill-primary" />
+        <CircleIcon className="text-primary absolute top-0 right-2 size-5 translate-x-1/2 -translate-y-1/2 fill-blue-500 stroke-white group-data-[state=unchecked]:hidden" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
