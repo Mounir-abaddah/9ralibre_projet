ffed83f0cb5fc10ae80981dbeb8bd1b3fed631b9
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-14 16:20:26 +0100

    :fire: clear: remove Microsoft button from OAuth component and update props to only include Google button

--- client/src/components/Oauth/OAuth.tsx
+++ client/src/components/Oauth/OAuth.tsx
@@ -1,10 +1,9 @@
-import { Google, Microsoft } from "developer-icons";
+import { Google } from "developer-icons";
 
 type OauthType = {
   text_1: string;
-  text_2: string;
 };
-const OAuth = ({ text_1, text_2 }: OauthType) => {
+const OAuth = ({ text_1 }: OauthType) => {
   const handleClick = ()=>{
     const apiUrl = import.meta.env.VITE_OAUTH_URL;
     window.location.href = apiUrl
@@ -12,15 +11,10 @@ const OAuth = ({ text_1, text_2 }: OauthType) => {
   return (
     <div className="w-full">
       <div className="flex w-full flex-col-reverse justify-around gap-3 md:flex-row lg:flex-row ">
-        <div onClick={handleClick} className="hover:inset-shadow-md flex w-full cursor-pointer items-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:bg-slate-50 dark:border-gray-300">
+        <div onClick={handleClick} className="hover:inset-shadow-md flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border p-2 font-medium transition-all duration-300 hover:bg-slate-50 dark:border-gray-300">
           <Google size={19}/>
           <hr className="h-6 rounded-md border-1" />
           <p>{text_1}</p>
-        </div>
-        <div className="hover:inset-shadow-md flex w-full cursor-pointer items-center gap-2 rounded-md border bg-black p-2 font-medium text-white transition-all duration-300">
-          <Microsoft size={19} />
-          <hr className="h-6 rounded-md border-1" />
-          <p>{text_2}</p>
         </div>
       </div>
       <div className="mt-3 flex w-full items-center justify-around gap-4">
