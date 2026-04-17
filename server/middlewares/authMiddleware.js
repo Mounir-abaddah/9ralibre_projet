ee1d7879e45e864ef7a9aa197c09a38f354f52ab
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-17 15:03:56 +0100

    :sparkles: feat: ajout du check si user est bloquer et il a passer la date dans authMiddleware

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,12 +1,24 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/UserModel");
 
-const authMiddleware = (req,res,next)=>{
+const authMiddleware = async (req,res,next)=>{
     const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
     if (!token) {
         return res.status(401).send({message: "⛔ Accès refusé : Token manquant",success: false});
     }
     try{
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await User.findById(decoded.userId).select("blockedUntil");
+        if (!user) {
+          return res.status(401).send({ message: "Utilisateur introuvable", success: false });
+        }
+        if (user.blockedUntil && user.blockedUntil > new Date()) {
+          return res.status(403).send({
+            message: "Votre compte est temporairement bloqué",
+            success: false,
+            blockedUntil: user.blockedUntil,
+          });
+        }
         req.user = decoded;
         next();
     }catch(err){
