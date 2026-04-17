8288acd200b821511ea69c551ed33829d536e18a
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-17 14:51:44 +0100

    :sparkles: feat: ajout des data dans app sidebar

--- client/src/components/Dashboard/Sidebar/app-sidebar.tsx
+++ client/src/components/Dashboard/Sidebar/app-sidebar.tsx
@@ -8,6 +8,10 @@ import {
   Video,
   MessageCircleMore,
   BookType,
+  Users,
+  Flag,
+  ScrollText,
+  MailWarning,
 } from "lucide-react"
 import { NavMain } from "@/components/Dashboard/Sidebar/nav-main"
 import { NavUser } from "@/components/Dashboard/Sidebar/nav-user"
@@ -20,8 +24,9 @@ import {
 } from "@/components/ui/sidebar"
 import logo from '@/assets/images/9ralibre.png'
 import { Collapsible } from "@/components/ui/collapsible"
+import { useProtectedRoutes } from "@/store/userStore"
 
-const data = {
+const dataProfesseur = {
   user: {
     name: "shadcn",
     email: "m@example.com",
@@ -62,7 +67,49 @@ const data = {
   ],
 }
 
+const dataAdmin = {
+   user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/admin/dashboard",
+      icon: Table2,
+      isActive: true,
+    },
+    {
+      title: "Utilisateur",
+      url: "/admin/users",
+      icon: Users,
+    },
+    {
+      title: "Signalement",
+      url: "/admin/signals",
+      icon: Flag,
+    },
+    {
+      title: "Moderation log",
+      url: "/admin/moderation-log",
+      icon: ScrollText,
+    },
+    {
+      title: "Appeals",
+      url: "/admin/appeals",
+      icon: MailWarning,
+    },
+  ],
+}
+
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const {data,fetchData} = useProtectedRoutes();
+
+  React.useEffect(()=>{
+    fetchData()
+  },[]);
+
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
@@ -71,13 +118,13 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <img src={logo} alt="logo_du_site" width={50}/>
             <div className="flex flex-col items-start group-data-[collapsible=icon]:hidden">
               <span className="truncate font-medium">9ralibre</span>
-              <span className="truncate text-xs">Espace Professeur</span>
+              <span className="truncate text-xs">{data?.role === "Professeur" ? 'Espace Professeur' : 'Espace Admin'}</span>
             </div>
           </div>
         </Collapsible>
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
+        <NavMain items={data?.role === "Professeur" ? dataProfesseur.navMain :  dataAdmin.navMain} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
