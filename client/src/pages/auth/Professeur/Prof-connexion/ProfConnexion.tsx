0f5e93d1faadb86081fbc7b6612650f7ecaffbe6
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-19 13:30:33 +0100

    :zap: feat: supprimation du public dans image dans profConnexion

--- client/src/pages/auth/Professeur/Prof-connexion/ProfConnexion.tsx
+++ client/src/pages/auth/Professeur/Prof-connexion/ProfConnexion.tsx
@@ -99,7 +99,7 @@ const ProfConnexion = () => {
   }
 
   return (
-    <div  className="relative flex min-h-screen items-center justify-center bg-[url('/public/backgorund_teacher.jpg')] bg-cover bg-no-repeat  px-4">
+    <div  className="relative flex min-h-screen items-center justify-center bg-[url('/backgorund_teacher.jpg')] bg-cover bg-no-repeat  px-4">
       {/* Logo */}
       <Link to={'/'}>
         <img
