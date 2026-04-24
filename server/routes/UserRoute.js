c94dc7f7002dd0990b289a6977879a1509082db7
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-24 16:19:50 +0100

    :sparkles: feat: ajout du matiereModel et niveaux et aussi ajout du route get matiere par le niveaux

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/UserModel");
+const NiveauxModel = require('../models/NiveauxModel');
+const MatiereModel = require('../models/MatiereModel')
 const VideosModel = require("../models/VideosModel");
 const CoursModels = require("../models/CoursModel");
 const QuizModel = require("../models/QuizModel");
@@ -30,6 +32,29 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+router.get("/fetch-matiere/:niveau", authMiddleware, async (req, res) => {
+  try {
+    const { niveau } = req.params;
+    const niveauDoc = await NiveauxModel.findOne({ nom: niveau });
+
+    if (!niveauDoc) {
+      return res.status(404).json({
+        message: "Niveau introuvable",
+      });
+    }
+    const matieres = await MatiereModel.find({
+      niveaux: niveauDoc._id,
+    });
+
+    res.status(200).json(matieres);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Erreur serveur",
+    });
+  }
+});
 
 router.get("/profile", authMiddleware, async (req, res) => {
   try {
@@ -739,4 +764,6 @@ router.get("/saved/latest", authMiddleware, async (req, res) => {
     });
   }
 });
+
+
 module.exports = router;
