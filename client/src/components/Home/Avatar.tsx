e544b52a87d486d381ac9e1284c4364115d15054
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-24 12:40:14 +0100

    :lipstick: style: ajout du width dans le composents Avatar

--- client/src/components/Home/Avatar.tsx
+++ client/src/components/Home/Avatar.tsx
@@ -59,7 +59,7 @@ const Avatare = ({ data }: typeAllData) => {
       {menuOpen &&
         createPortal(
           <div
-            className="fixed top-[50px] right-[60px] z-[9999] mt-4 w-64 origin-top-right pt-2"
+            className="fixed top-[50px] right-[60px] z-[9999] mt-4 w-68 origin-top-right pt-2"
             onMouseEnter={() => setMenuOpen(true)}
             onMouseLeave={() => setMenuOpen(false)}
           >
