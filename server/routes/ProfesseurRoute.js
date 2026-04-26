424e0b725d9b1c7c7829dbd903225e6597ea956d
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-26 12:09:55 +0100

    :sparkles: feat: augmentation de la limite de cours récupérés à 16 dans la route getCours

--- server/routes/ProfesseurRoute.js
+++ server/routes/ProfesseurRoute.js
@@ -178,7 +178,7 @@ router.get("/getCours", authMiddlewares, profMiddleware, async (req, res) => {
     const { semestre, type, filiere, search } = req.query;
 
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 6;
+    const limit = parseInt(req.query.limit) || 16;
     const skip = (page - 1) * limit;
 
     const queryObject = {
