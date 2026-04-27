ee498681838d226518a76166bba6b4f1090689d8
Mounir-abaddah <mounirabaddah6@gmail.com>
2026-04-27 14:41:00 +0100

    :fire: clear: supprimation du Main item et aussi les item

--- client/src/pages/auth/DrawExcalidraw/DrawExcalidraw.tsx
+++ client/src/pages/auth/DrawExcalidraw/DrawExcalidraw.tsx
@@ -1,7 +1,6 @@
 import { useTheme } from "@/context/ThemeContext";
-import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
+import { Excalidraw, WelcomeScreen } from "@excalidraw/excalidraw";
 import "@excalidraw/excalidraw/index.css";
-import { GitHubDark, Google } from "developer-icons";
 import { useEffect, useState } from "react";
 
 export default function DrawExcalidraw() {
@@ -28,20 +27,6 @@ export default function DrawExcalidraw() {
         onChange={(updatedElements) => setElements(updatedElements)}
       >
         <WelcomeScreen />
-        <MainMenu>
-          <MainMenu.Group title="Excalidraw items">
-            <MainMenu.DefaultItems.Socials />
-            <MainMenu.DefaultItems.Export />
-          </MainMenu.Group>
-          <MainMenu.Group title="9ralibre items">
-          <MainMenu.ItemLink href="https://google.com">
-            <Google size={12}/> Google
-          </MainMenu.ItemLink>
-          <MainMenu.ItemLink href="https://github.com/Mounir-abaddah">
-            <GitHubDark size={12}/> Github
-          </MainMenu.ItemLink>
-          </MainMenu.Group>
-        </MainMenu>
       </Excalidraw>
     </div>
   );
